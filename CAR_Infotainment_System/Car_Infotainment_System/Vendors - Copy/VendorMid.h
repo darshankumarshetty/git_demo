2899b07b864b4caff2e374e0ad4ee332f4d1d7d5
darshankumarshetty <darshankumarshetty55@gmail.com>
2022-06-25 12:44:53 +0530

    updated

---
+++ CAR_Infotainment_System/Car_Infotainment_System/Vendors - Copy/VendorMid.h
@@ -0,0 +1,13 @@
+#ifndef VENDORMID_H
+#define VENDORMID_H
+
+#include "INeedVendorData.h"
+
+class VendorMid : public INeedVendorData
+{
+public:
+    VendorMid();
+    ~VendorMid();
+};
+
+#endif // VENDORMID_H
---
+++ CAR_Infotainment_System/Car_Infotainment_System/Vendors - Copy/VendorTop.h
@@ -0,0 +1,13 @@
+#ifndef VENDORTOP_H
+#define VENDORTOP_H
+
+#include "INeedVendorData.h"
+
+class VendorTop : public INeedVendorData
+{
+public:
+    VendorTop();
+    ~VendorTop();
+};
+
+#endif // VENDORTOP_H
